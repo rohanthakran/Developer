905af8a30ceed9a82599a9cec22ae4f569d11850
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-07-28 14:46:37 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const { check, validationResult } = require('express-validator');
+
 
 exports.signout = (req,res) =>{
     res.json({
@@ -6,6 +8,14 @@ exports.signout = (req,res) =>{
 })}
 
 exports.signup = (req,res) =>{
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg 
+        })
+    }
    const user = new User(req.body);
    user.save( (err,user) =>{
         if(err){
