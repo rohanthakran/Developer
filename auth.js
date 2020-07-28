2746bcc972f0808bf2fc43928c6ed51194fcf8c7
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-07-28 14:08:22 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,16 +1,23 @@
-const mongoose = require("mongoose");
+const User = require("../models/user");
 
-const categorySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      maxlength: 32,
-      unique: true
-    }
-  },
-  { timestamps: true }
-);
+exports.signout = (req,res) =>{
+    res.json({
+        message : " User "
+})}
 
-module.exports = mongoose.model("Category", categorySchema);
+exports.signup = (req,res) =>{
+   const user = new User(req.body);
+   user.save( (err,user) =>{
+        if(err){
+            return res.status(400).json({
+                err: "NOT able to save user in DB"
+            })
+        }
+        res.json({
+            name: user.name,
+            email: user.email,
+            id: user._id
+        })
+   })
+}
+
